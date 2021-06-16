22f6875356dda061219954f33297b988addefb6b
Fla88vus <flaviacardosi@gmail.com>
2021-06-16 13:12:52 +0200

    completed Task manager with express

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,34 @@
-console.log('Task Manager App')
+console.log("Task Manager App");
+const express = require("express");
+
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const erroHandlerMiddleware = require("./middleware/error-handler");
+// middleware
+app.use(express.static("./public"));
+app.use(express.json());
+app.use(notFound);
+app.use(erroHandlerMiddleware);
+// routes
+
+// app.get("/hello", (req, res) => {
+//   res.send("Task Manager App");
+// });
+
+app.use("/api/v1/tasks", tasks);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}`));
+    // console.log("CONNECT TO THE DB");
+  } catch (error) {
+    console.log(error);
+  }
+};
+const port = process.env.PORT || 3000;
+
+start();
---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,95 @@
+const Task = require("../models/Task");
+const asyncWrapper = require("./middleware/async");
+
+const getAllTasks = asyncWrapper(async (req, res) => {
+  // look mongoose queries documentation CRUD operations
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+
+  // res.send("all items");
+});
+
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+    // res.json({ id: req.params.id });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findByIdAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+    // on FE i would like to display all task list withoute the deleted task
+    // res.status(200).json({task:null, status: success})
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+  // res.send("delete task");
+};
+
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    // {new:true, runValidators:true} === options to convalidate the schema of Task ex. empty string
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+  // res.send("update single task");
+};
+const editTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+      overWrite: true,
+    });
+
+    // {new:true, runValidators:true} === options to convalidate the schema of Task ex. empty string
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+  // res.send("update single task");
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+  editTask,
+};
